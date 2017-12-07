52e3a2f459c706b81f7f4b817e9b1c6518de4319
SergiRomero <sergi9rom@gmail.com>
2017-12-07 12:55:49 +0100

    Esplai Fent-se

---
+++ scripts/grups.js
@@ -0,0 +1,129 @@
+"use strict";
+function showMinis() {
+    
+    var grup = document.getElementsByClassName('grup');
+    
+    if (grup[0].style.display != 'none') {
+        
+        hideAll();
+        
+    } else {
+        
+        grup[0].style.display = 'block';
+        grup[1].style.display = 'none';
+        grup[2].style.display = 'none';
+        grup[3].style.display = 'none';
+        grup[4].style.display = 'none';
+        grup[5].style.display = 'none';
+        
+    }
+};
+
+function showPetits() {
+    
+    var grup = document.getElementsByClassName('grup');
+    
+    if (grup[1].style.display != 'none') {
+        
+        hideAll();
+        
+    } else {
+        
+        grup[0].style.display = 'none';
+        grup[1].style.display = 'block';
+        grup[2].style.display = 'none';
+        grup[3].style.display = 'none';
+        grup[4].style.display = 'none';
+        grup[5].style.display = 'none';
+        
+    }
+};
+
+function showMitjans() {
+    
+    var grup = document.getElementsByClassName('grup');
+    
+    if (grup[2].style.display != 'none') {
+        
+        hideAll();
+        
+    } else {
+        
+        grup[0].style.display = 'none';
+        grup[1].style.display = 'none';
+        grup[2].style.display = 'block';
+        grup[3].style.display = 'none';
+        grup[4].style.display = 'none';
+        grup[5].style.display = 'none';
+        
+    }
+};
+
+function showGrans() {
+    
+    var grup = document.getElementsByClassName('grup');
+    
+    if (grup[3].style.display != 'none') {
+        
+        hideAll();
+        
+    } else {
+        
+        grup[0].style.display = 'none';
+        grup[1].style.display = 'none';
+        grup[2].style.display = 'none';
+        grup[3].style.display = 'block';
+        grup[4].style.display = 'none';
+        grup[5].style.display = 'none';
+        
+    }
+};
+
+function showJoves1() {
+    
+    var grup = document.getElementsByClassName('grup');
+    
+    if (grup[4].style.display != 'none') {
+        
+        hideAll();
+        
+    } else {
+        
+        grup[0].style.display = 'none';
+        grup[1].style.display = 'none';
+        grup[2].style.display = 'none';
+        grup[3].style.display = 'none';
+        grup[4].style.display = 'block';
+        grup[5].style.display = 'none';
+        
+    }
+};
+
+function showJoves2() {
+    
+    var grup = document.getElementsByClassName('grup');
+    
+    if (grup[5].style.display != 'none') {
+        
+        hideAll();
+        
+    } else {
+        
+        grup[0].style.display = 'none';
+        grup[1].style.display = 'none';
+        grup[2].style.display = 'none';
+        grup[3].style.display = 'none';
+        grup[4].style.display = 'none';
+        grup[5].style.display = 'block';
+        
+    }
+};
+
+function hideAll() {
+    document.getElementsByClassName('grup')[0].style.display = 'none';
+    document.getElementsByClassName('grup')[1].style.display = 'none';
+    document.getElementsByClassName('grup')[2].style.display = 'none';
+    document.getElementsByClassName('grup')[3].style.display = 'none';
+    document.getElementsByClassName('grup')[4].style.display = 'none';
+    document.getElementsByClassName('grup')[5].style.display = 'none';
+};
