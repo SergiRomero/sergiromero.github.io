c29ed3ae7e01da2a3478133d3340c74236f8a7dc
SergiRomero <sergi9rom@gmail.com>
2017-12-15 21:13:39 +0100

    Revision I comentario de Codigo

--- scripts/grups.js
+++ scripts/grups.js
@@ -1,9 +1,12 @@
 "use strict";
 
-var grup = document.getElementsByClassName('grup');
-var button = document.getElementsByTagName('button');
+var grup = document.getElementsByClassName('grup'); //Lista de grupos 
+var button = document.getElementsByTagName('button'); //Lista de botones 
 
-
+/**
+ * Itera por todos los grupos actualizando su visibilidad y el tipo de letra del boton asociado
+ * @param j, grupo que ha de quedar visible (ver tabla de relaciones en .html sobre el que se ejecuta)
+ */
 function showSelected(j) {
 
     for (var i = 0; i < grup.length; i ++){
